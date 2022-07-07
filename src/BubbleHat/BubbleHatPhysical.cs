39fd5367b81a5ee8e59941485f83f32cccb70af4
kadw108 <wallwritinglast@gmail.com>
2022-07-06 23:44:02 -0400

    BubbleHat basics complete

--- src/BubbleHat/BubbleHatPhysical.cs
+++ src/BubbleHat/BubbleHatPhysical.cs
@@ -6,8 +6,9 @@ namespace HatWorld
     sealed class BubbleHatPhysical : HatPhysical
     {
         // Constants for sLeaser sprite index (higher index appears over lower)
-        public const int neckIndex = 0;
-        public const int edgeIndex = 1;
+		public const int glassIndex = 0;
+        public const int neckIndex = 1;
+        public const int edgeIndex = 2;
 
 		public override HatType hatType => HatType.Bubble;
 
@@ -15,9 +16,10 @@ namespace HatWorld
 
         public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
         {
-			sLeaser.sprites = new FSprite[2];
-            sLeaser.sprites[neckIndex] = new FSprite("SpearRag", true);
-            sLeaser.sprites[edgeIndex] = new FSprite("LizardBubble7", true);
+            sLeaser.sprites = new FSprite[3];
+            sLeaser.sprites[neckIndex] = new FSprite("SpearFragment2", true) { scale = 1.1f }; // neck collar
+            sLeaser.sprites[edgeIndex] = new FSprite("LizardBubble7", true) { scale = 1.3f }; // edge of bubble
+            sLeaser.sprites[glassIndex] = new FSprite("Circle20", true) { scale = 1.1f }; // inside of bubble
 
             this.AddToContainer(sLeaser, rCam, null);
         }
@@ -30,14 +32,19 @@ namespace HatWorld
             {
                 sLeaser.sprites[j].rotation = hatRotation;
             }
+			drawPos -= upDir * 4;
 
-            /* Set positions */
-            sLeaser.sprites[neckIndex].SetPosition(drawPos);
-            sLeaser.sprites[edgeIndex].SetPosition(drawPos + upDir * 5f);
+			sLeaser.sprites[neckIndex].SetPosition(drawPos + upDir * -10);
+
+			sLeaser.sprites[edgeIndex].SetPosition(drawPos);
+			sLeaser.sprites[glassIndex].SetPosition(drawPos);
         }
 
         public override void ApplyPalette(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
         {
+			sLeaser.sprites[neckIndex].color = new Color(0.74f, 0.83f, 0.90f);
+			sLeaser.sprites[edgeIndex].color = new Color(0.57f, 0.79f, 0.94f);
+			sLeaser.sprites[glassIndex].color = new Color(0.94f, 0.91f, 1f, 0.5f);
         }
     }
 }
